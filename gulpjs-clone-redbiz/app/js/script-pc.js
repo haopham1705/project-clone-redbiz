a84eb7944ed73df52ffa4bc15951042b2928ec02
haopham1705 <phamvanhao009@gmail.com>
2021-11-24 20:42:59 +0700

    build prj with gulpjs

--- gulpjs-clone-redbiz/app/js/script-pc.js
+++ gulpjs-clone-redbiz/app/js/script-pc.js
@@ -15,3 +15,34 @@ function closeNav() {
     // document.body.style.backgroundColor = "#fff";
 
 }
+
+var swiperSlider = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiperClient = new Swiper(".mySwiperCarousel", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+    }
+});
+
+swiperSlider();
+swiperClient();
