4485823f36ec108289c0092ad464b3096a1d2afc
haopham1705 <phamvanhao009@gmail.com>
2021-12-13 14:52:51 +0700

    add style css

--- gulpjs-clone-redbiz/app/js/script-pc.js
+++ gulpjs-clone-redbiz/app/js/script-pc.js
@@ -1,17 +1,13 @@
 
 // Side Nabar action
 function openNav() {
-    document.getElementById("mySidenav").style.width = "80vw";
-    // document.getElementById("app").style.marginLeft = "250px";
-    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("overlay-navbar").style.display = "block";
-    // document.getElementById("app").style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.width = "80vw"; 
+    document.getElementById("overlay-navbar").style.display = "block"; 
 } 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("app").style.marginLeft = "0";
-    document.getElementById("overlay-navbar").style.display = "none";
-    // document.body.style.backgroundColor = "#fff";
+    document.getElementById("overlay-navbar").style.display = "none"; 
 } 
 
 // home-head-slider
@@ -46,26 +42,26 @@ var swiperClient = new Swiper(".mySwiperCarousel", {
 
 /* Start scroll go top js
 ============================================================================================ */
-var prevScrollpos = window.pageYOffset;
-function checkScrollGoTop() {
+// var prevScrollpos = window.pageYOffset;
+// function checkScrollGoTop() {
 
-    // Start effect fade in btn-go-top
-    if ($(this).scrollTop() <= 500) {
-        $('.scroll-to-top').fadeOut();
-    } else {
-        $('.scroll-to-top').fadeIn();
-    }
+//     // Start effect fade in btn-go-top
+//     if ($(this).scrollTop() <= 500) {
+//         $('.scroll-to-top').fadeOut();
+//     } else {
+//         $('.scroll-to-top').fadeIn();
+//     }
 
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        $('#btn_gotop').fadeIn('300');
-        document.querySelector("#btn_gotop").style.opacity = "1";
-    } else {
-        document.querySelector("#btn_gotop").style.opacity = "0";
-        $('#btn_gotop').fadeOut('fast');
-    }
-    prevScrollpos = currentScrollPos;
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         $('#btn_gotop').fadeIn('300');
+//         document.querySelector("#btn_gotop").style.opacity = "1";
+//     } else {
+//         document.querySelector("#btn_gotop").style.opacity = "0";
+//         $('#btn_gotop').fadeOut('fast');
+//     }
+//     prevScrollpos = currentScrollPos;
     // End effect fade in btn-go-top
-}
+// }
     /* End scroll go top js
 ============================================================================================ */
